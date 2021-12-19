bd23aa8fe3559e3faf55b54e19c275880189d1ff
Alvin Yearby <leroy.yearby@gmail.com>
2021-12-19 01:20:06 -0500

    passes Engineer.test

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,19 @@
+const Engineer = require('../lib/Engineer'); 
+
+test('creates an engineer', () => {
+    const engineer = new Engineer('Alvin', 10, 'leroy.yearby@gmail.com', 'AlvinY4');
+
+    expect(engineer.github).toEqual(expect.any(String)); 
+});
+
+test('gets engineer github', () => {
+    const engineer = new Engineer('Alvin', 10, 'leroy.yearby@gmail.com', 'AlvinY4');
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString())); 
+
+});
+
+test('gets employee role', () => {
+    const engineer = new Engineer('Alvin', 10, 'leroy.yearby@gmail.com', 'AlvinY4'); 
+
+    expect(engineer.getRole()).toEqual("Engineer"); 
+}); 
